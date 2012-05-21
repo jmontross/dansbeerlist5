7151c3dbee8fb0c72139c983407b47c547486748
Dan Warter <dan@dansbeerlist.com>
2012-05-21 15:43:55 -0700

    production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Dansbeerlist::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.action_mailer.default_url_options = { :host => 'http://dansbeerlist2.herokuapp.com' }
+ config.action_mailer.default_url_options = { :host => 'localhost' }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Dansbeerlist::Application.configure do
+  
+  config.action_mailer.default_url_options = { :host => 'http://dansbeerlist2.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
