9797fc93184048f036acb32eed1c4cfb444c4a29
Dan Warter <dan@dansbeerlist.com>
2012-05-21 15:46:06 -0700

    production again

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-config.action_mailer.default_url_options = { :host => 'http://dansbeerlist2.herokuapp.com' }
+
 
 # Initialize the rails application
 Dansbeerlist::Application.initialize!
