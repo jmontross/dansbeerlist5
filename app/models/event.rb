daa8b5341f844a522de263ab83322b03b2c44a01
Dan Warter <dan@dansbeerlist.com>
2012-06-07 12:57:59 -0700

    changed back

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,5 +6,5 @@ class Event < ActiveRecord::Base
 	:storage => :s3,
 	:s3_credentials => "#{Rails.root}/config/s3.yml",
 	:path => "dansbeerlist/:id/:basename.:extention"
-	:order => "record_events.index DESC"
+	default_scope :order => 'events.created_at DESC'
 end
