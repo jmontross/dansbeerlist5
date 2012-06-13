81c220d0347a921f790ca912c08079f3cedc36a3
Dan Warter <dan@dansbeerlist.com>
2012-06-12 22:04:22 -0700

    first attempt at mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,18 @@ Dansbeerlist::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:
+  3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :enable_startttls_auto => true,
+      :address => 'smtp.gmail.com',
+      :port => '587',
+      :authentication => :plain,
+      :domain => 'gmail.com.com',
+      :user_name => 'danielpwarter@gmail.com',
+      :password => 'dabears6874'
+    }
+  
 end
