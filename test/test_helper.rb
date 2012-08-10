7fa7939914c085d05072182fe65c58777df0b11d
Joshua Montross <jmontross@empowered.com>
2012-08-09 20:26:10 -0700

    adds tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,11 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
+  def admin_user
+    user = {"username"=>"Dan", "email"=>"dan@dan.com", "hometown"=>"no", "beer"=>"pale ale", "skill"=>"jmontross@empowered.com", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]"} 
+    @admin_user = User.new(:admin=>"true")    
+    return @admin_user
+  end
+
   # Add more helper methods to be used by all tests here...
 end
--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,6 +1,17 @@
-require 'test_helper'
+
+require '../test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+  
+  
+  test "article creation " do 
+    admin_user
+    puts "we has admin user #{@admin_user}"
+    # test = Post.new
+    #     puts test
+    #     test.assert == "stuff"
+  end
+  
   # test "the truth" do
   #   assert true
   # end
